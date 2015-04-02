271bee9ea5677788fcc057d17475423d344ccef2
f_roseberry@live.fr <f_roseberry@live.fr>
2015-04-01 20:36:05 -0400

    Added a first test

---
+++ TestProject/CameraTest.cs
@@ -0,0 +1,78 @@
+﻿using TileEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    
+    
+    /// <summary>
+    ///This is a test class for CameraTest and is intended
+    ///to contain all CameraTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class CameraTest
+    {
+        private TestContext testContextInstance;
+        private IMouseInput input = null;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Default
+        ///</summary>
+        [TestMethod()]
+        public void DefaultCameraShouldBePositionedAtOrigin()
+        {
+            Camera actual = Camera.Default(input);
+
+            Assert.AreEqual(0, actual.X);
+            Assert.AreEqual(0, actual.Y);
+        }
+    }
+}
