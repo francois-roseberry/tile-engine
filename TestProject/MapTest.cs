a209f8cc8f8a575922d896b1940bac57b2976562
f_roseberry@live.fr <f_roseberry@live.fr>
2015-04-02 20:22:08 -0400

    Added tests for Map

---
+++ TestProject/MapTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TileEngine;
+
+namespace TestProject
+{
+    /// <summary>
+    /// Summary description for MapTest
+    /// </summary>
+    [TestClass]
+    public class MapTest
+    {
+        private Map map;
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestInitialize()]
+        public void Setup() {
+            map = new Map();
+        }
+
+        [TestMethod]
+        public void WidthShouldBeNumberOfTilesPerRowTimesTileWidth()
+        {
+            Assert.AreEqual((Map.NB_TILES_PER_ROW - 1) * Map.TILE_WIDTH, map.Width);
+        }
+
+        [TestMethod]
+        public void HeightShouldBeNumberOfTilesPerColumnTimesQuarterOfTileHeight()
+        {
+            Assert.AreEqual((Map.NB_TILES_PER_COLUMN - 1) * Map.TILE_HEIGHT / 4, map.Height);
+        }
+
+        [TestMethod]
+        public void ShouldHaveRightNumberOfTiles()
+        {
+            Assert.AreEqual(Map.NB_TILES_PER_ROW * Map.NB_TILES_PER_COLUMN, map.Tiles.Count);
+        }
+
+        [TestMethod]
+        public void AllTilesShouldHaveGrassType()
+        {
+            Assert.IsTrue(map.Tiles.All(tile => tile.Type == TileType.Grass));
+        }
+    }
+}
--- TileEngine/TileEngine/Map.cs
+++ TileEngine/TileEngine/Map.cs
@@ -13,25 +13,25 @@ namespace TileEngine
         public const int TILE_WIDTH = 64;
         public const int TILE_HEIGHT = 64;
 
-        private const int NB_TILES_WIDTH = 20;
-        private const int NB_TILES_HEIGHT = 50;
+        public const int NB_TILES_PER_ROW = 20;
+        public const int NB_TILES_PER_COLUMN = 50;
 
         private List<Tile> tiles = new List<Tile>();
 
         public Map()
         {
-            for (int x = 0; x < NB_TILES_WIDTH; x++)
+            for (int x = 0; x < NB_TILES_PER_ROW; x++)
             {
-                for (int y = 0; y < NB_TILES_HEIGHT; y++)
+                for (int y = 0; y < NB_TILES_PER_COLUMN; y++)
                 {
                     tiles.Add(new Tile(x, y, TileType.Grass));
                 }
             }
         }
 
-        public int Width { get { return (NB_TILES_WIDTH - 1) * TILE_WIDTH; } }
+        public int Width { get { return (NB_TILES_PER_ROW - 1) * TILE_WIDTH; } }
 
-        public int Height { get { return (NB_TILES_HEIGHT - 1) * TILE_HEIGHT/4; } }
+        public int Height { get { return (NB_TILES_PER_COLUMN - 1) * TILE_HEIGHT/4; } }
 
         public List<Tile> Tiles { get { return tiles; } }
     }
